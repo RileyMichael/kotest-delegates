1e9aa54ea5140dde112ebd34f3d29856ce4381e0
RileyMichael <rmcgee@clearcover.com>
2022-10-17 15:29:32 -0500

    upgrade to 5.x.x and reproduce issue

--- src/test/kotlin/ExampleSpec.kt
+++ src/test/kotlin/ExampleSpec.kt
@@ -1,11 +1,11 @@
+import io.kotest.core.names.TestName
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.scopes.FreeSpecContainerContext
-import io.kotest.core.test.TestType
+import io.kotest.core.spec.style.scopes.FreeSpecContainerScope
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.runBlocking
 import kotlin.reflect.KProperty
 
-class ExampleSpec: FreeSpec({
+class ExampleSpec : FreeSpec({
   "a example test" - {
     `some assertion`
   }
@@ -15,15 +15,19 @@ val Assertions.`some assertion` by reusableTest {
   1 + 1 shouldBe 2
 }
 
-typealias Assertions = FreeSpecContainerContext
+typealias Assertions = FreeSpecContainerScope
+
 fun reusableTest(block: suspend () -> Unit) = ReusableTestDelegate(block)
 
 class ReusableTestDelegate(private val block: suspend () -> Unit) {
-  operator fun getValue(thisRef: FreeSpecContainerContext?, property: KProperty<*>) {
+  operator fun getValue(thisRef: FreeSpecContainerScope?, property: KProperty<*>) {
     val functionName = getFunctionNameForTest()
-    if (thisRef != null) runBlocking { thisRef.addTest(functionName, TestType.Test) { block() } }
+    if (thisRef != null) runBlocking {
+      // this blocks
+      thisRef.registerTest(functionName, false, null) { block() }
+    }
   }
 }
 
 // some logic to determine test name from stacktrace
-fun getFunctionNameForTest() = "a test"
+fun getFunctionNameForTest() = TestName("a test")
